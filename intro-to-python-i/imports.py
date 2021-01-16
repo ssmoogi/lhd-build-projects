263e0d8ee04dace436a8552f928ec13359009b4d
ssmoogi <ssmoogi@gmail.com>
2021-01-16 01:43:40 -0800

    add new files

---
+++ intro-to-python-i/imports.py
@@ -0,0 +1,7 @@
+import returns
+# from returns import create_full_name
+import random
+
+print(returns.create_full_name("Sahana", "Moogi"))
+
+print(random.randint(0, 100))
---
+++ intro-to-python-i/returns.py
@@ -0,0 +1,9 @@
+# function name: create_full_name
+# parameter names: first_name, last_name
+
+def create_full_name(first_name, last_name):
+  full_name = first_name.upper() + " " + last_name.upper()
+  return full_name
+
+result = create_full_name("Sahana", "Moogi")
+# print(result)
